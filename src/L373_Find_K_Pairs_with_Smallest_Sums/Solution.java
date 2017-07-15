d5cb235331c103ce7c4696b3e7009608fe47d501
he <ffpphh@outlook.com>
2017-07-15 21:29:00 +0800

    L373

--- src/L373_Find_K_Pairs_with_Smallest_Sums/Solution.java
+++ src/L373_Find_K_Pairs_with_Smallest_Sums/Solution.java
@@ -6,57 +6,52 @@ import java.util.*;
  * Created by he on 17-6-3.
  */
 public class Solution {
-    public class MyComparator implements Comparator<int[]> {  //实现比较器
-        @Override
-        public int compare(int[] nums1, int[] nums2) {
-            int sum1 = nums1[0] + nums1[1];
-            int sum2 = nums2[0] + nums2[1];
-            if(sum1 > sum2)
-                return 1;
-            else
-                return -1;
-        }
-    }
-    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<int []> res = new LinkedList<>();
-        Map<int[], Integer> sumPair = new TreeMap<int[], Integer>(new MyComparator());
-        for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-
-                int[] tmp = new int[2];
-                tmp[0] = nums1[i];
-                tmp[1] = nums2[j];
-                System.out.println("(" + String.valueOf(tmp[0]) + ", " + String.valueOf(tmp[1]) + ")");
-                Integer numtmp = sumPair.get(tmp);
-                if(numtmp != null)
-                    sumPair.put(tmp, numtmp + 1);
-                else sumPair.put(tmp, 1);
-            }
-        }
-        Set<int[]> keySet = sumPair.keySet();
-        Iterator<int[]> iterable = keySet.iterator();
-        int count = 0;
-        System.out.println(sumPair.toString());
-        while (iterable.hasNext() && count < k){
-            int[] tmpPair = iterable.next();
-            Integer pairNum = sumPair.get(tmpPair);
-            System.out.println(pairNum);
-            while (pairNum > 0) {
-                res.add(tmpPair);
-                count++;
-                pairNum--;
-                if(count >= k){
-                    break;
-                }
-            }
-        }
+public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    List<int []> res = new LinkedList<>();
+    if(nums1.length == 0 || nums2.length == 0)
         return res;
+    boolean isSwap = false;
+    if (nums1.length > nums2.length){
+        int[] tmp = nums2;
+        nums2 = nums1;
+        nums1 = tmp;
+        isSwap = true;
     }
-
+    int[] nums2Indexs = new int[nums1.length];
+    int[] sumValue = new int[nums1.length];
+    for(int i = 0; i < nums1.length; i++){
+        nums2Indexs[i] = 0;
+        sumValue[i] = nums1[i] + nums2[0];
+    }
+    int MAXSUM = nums1[nums1.length - 1] + nums2[nums2.length - 1] + 1;
+    int num1Index = 0;
+    while(res.size() < k){
+        int minValIndex = 0;
+        for(int i = 0; i < nums1.length; i++){
+            if(sumValue[i] < sumValue[minValIndex]){
+                minValIndex = i;
+            }
+        }
+        num1Index = minValIndex;
+        if(sumValue[num1Index] == MAXSUM) break;
+        if (isSwap) {
+            int[] tmp = {nums2[nums2Indexs[num1Index]], nums1[num1Index]};
+            res.add(tmp);
+        } else{
+            int[] tmp = {nums1[num1Index], nums2[nums2Indexs[num1Index]]};
+            res.add(tmp);
+        }
+        nums2Indexs[num1Index] ++;
+        if(nums2Indexs[num1Index] < nums2.length)
+            sumValue[num1Index] = nums1[num1Index] + nums2[nums2Indexs[num1Index]];
+        else sumValue[num1Index] = MAXSUM;
+    }
+    return res;
+}
     public static void main(String[] args) {
         int[] nums1 = {1,1,2};
         int[] nums2 = {1,2,3};
         Solution solution = new Solution();
-        System.out.println(solution.kSmallestPairs(nums1, nums2, 100).toString());
+        System.out.println(solution.kSmallestPairs(nums1, nums2, 2).toString());
     }
 }
